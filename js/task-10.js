e6e7847f698b2c3b7a5d04579d09a15c2cc621f6
Oleksandr <live.gp@gmail.com>
2023-04-27 22:48:07 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,47 @@
+const refs = {
+	controls: document.querySelector('#controls'),
+	input: document.querySelector('#controls input'),
+	boxes: document.querySelector('#boxes'),
+	createBtn: document.querySelector('[data-create]'),
+	destroyBtn: document.querySelector('[data-destroy]'),
+};
+
+refs.createBtn.addEventListener('click', createClick);
+refs.destroyBtn.addEventListener('click', destroyBoxes);
+refs.input.addEventListener('input', inputChange);
+
+let amount = 0;
+const size = 30;
+const step = 10;
+const boxes = [];
+
+function createClick() {
+	amount = refs.input.value;
+	createBoxes(amount);
+}
+
+function destroyBoxes() {
+	refs.boxes.innerHTML = '';
+}
+
+function inputChange() {
+	amount = refs.input.value;
+}
+
+function createBoxes(amount) {
+	for (let i = 0; i < amount; i += 1) {
+		const box = document.createElement('div');
+		box.style.width = `${size + step * i}px`;
+		box.style.height = `${size + step * i}px`;
+		box.style.backgroundColor = getRandomHexColor();
+		boxes.push(box);
+	}
+
+	refs.boxes.append(...boxes);
+}
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
 }
