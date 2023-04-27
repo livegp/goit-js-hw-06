3fe2c07957f31304fa70bb56386fdcbbf7b9a840
Oleksandr <live.gp@gmail.com>
2023-04-27 21:41:56 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,18 @@
+const refs = {
+	btn: document.querySelector('.change-color'),
+	body: document.querySelector('body'),
+	span: document.querySelector('.color'),
+};
+
+refs.btn.addEventListener('click', changeColor);
+
+function changeColor() {
+	refs.body.style.backgroundColor = getRandomHexColor();
+	refs.span.textContent = getRandomHexColor();
+}
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+	return `#${Math.floor(Math.random() * 16777215)
+		.toString(16)
+		.padStart(6, 0)}`;
 }
